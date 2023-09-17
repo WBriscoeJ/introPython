ead6c2b46ed7225c16de30f5cf7cc68374fa4141
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-09-17 18:25:11 +0000

    done quiz1

---
+++ introPythonActivies/readingActivity.py
@@ -0,0 +1,4 @@
+# raed the follwoing on Python funtion
+"http:// www.freecodingcamp.org/news/functions-in-python-abeginners-guid"
+
+# 
--- quizes/quiz1.py
+++ quizes/quiz1.py
@@ -4,10 +4,10 @@ has to start with a letter or underscore if its a number or anything else its wr
 # a situation where you would use each one. 3 operators are Comparison,Logical,and Assignment operators
 For comparison operator we can assert whether two values or expressions are equal.For Logical operators
 I am a basketball player which is true .I am not smart which is false.
-For assignments A = 5; // use Assignment symbol to assign 5 to the operand A
 
 # 3. Describe the difference between camal case and pascal case.
 pascal case requires the first letter of the first word to also be capitalized.
+
 # 4. Which operator is being used in the example below
 numberOfApples = 10
 numberOfOranges = 12
@@ -21,16 +21,26 @@ numberOfApples < numberOfOranges =22 The operator being used is  comparison oper
 familymembergivesgiftcard=100
 anotherFamilymemberGivesGiftCard=$50.00
 friendGivesGiftCard=$35.50
-chang
-familyMemberGivesGiftGard<anotherFamilymemberGivesGiftCard<friendGivesGiftCard=$185.5
+
+familyMemberGivesGiftGard < anotherFamilymemberGivesGiftCard < friendGivesGiftCard=$185.5
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
-# should ask a user to enter their name.nameofbasketballteam  
+# should ask a user to enter their name.nameofbasketballteam() 
+numberOfBasketballteam=(_Warriors)
+print=(Warriors)
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
 # They want to know a visitor's full name, their age, if they have an allergy, and what their 
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
-# and use the input() function and print function to take in and print out the user's responses. 
+# and use the input() function and print function to take in and print out the user's responses.
+# nameOfVisitor's=(_BLCommunity)
+# nameTheirAge=(thirteenandolder)
+# numberOfAllergys(zeroallergys)
+#nameOfFavoriteFood=(Fried chicken,mac and cheese,French fries and hamburgers).
+
+print(_BLCommunity,thirteenandolder,zeroallergys,and
+      Fried chicken,mac and cheese,French fries and hamburgers)
 
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
@@ -38,6 +48,8 @@ familyMemberGivesGiftGard<anotherFamilymemberGivesGiftCard<friendGivesGiftCard=$
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
 
-# 9. In your own words, describe what a program is?
+# 9. In your own words, describe what a program is?A program is a set of instrucctions that tells
+you what to do in a specific order
 
-# 10. Describe the difference between a float and an integer. 
+# 10. Describe the difference between a float and an integer.A integer is a number without a decimal.
+# A float is a number that has a decimal.
