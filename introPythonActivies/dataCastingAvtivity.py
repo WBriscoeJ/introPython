2ce066544ba38828596d638376284e86213eccc9
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-09-20 17:04:12 +0000

    doneActivity

---
+++ introPythonActivies/dataCastingAvtivity.py
@@ -0,0 +1,22 @@
+# convert the following datatypes into floats
+numberOne=12
+numberTwo=100
+print(float (numberOne))
+print(float (numberTwo))
+# cconvert the following floats into integers.
+NumberThree=12.34
+NJumberFour=23.33
+print(int(NumberThree))
+print(int(NumberFour))
+# modify the code below to print an integer number.
+num_one=20
+num_two=23.45
+
+print(num_one + num_two()
+
+# modify the following code below to work correctly.
+name_of_pet="Spuny"
+age_of_pet="10"
+
+print("this is my dog",+ name_ of_pet +".He is"+ age_of_pet +"years old.")
+
