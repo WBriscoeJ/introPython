a99ea78980326cfc9dbe6f9b5bc559670a995d99
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-09-22 15:55:57 +0000

    done dataCastingAvtivity.py

--- introPythonActivies/dataCastingAvtivity.py
+++ introPythonActivies/dataCastingAvtivity.py
@@ -4,19 +4,19 @@ numberTwo=100
 print(float (numberOne))
 print(float (numberTwo))
 # cconvert the following floats into integers.
-NumberThree=12.34
-NJumberFour=23.33
+numberThree=12.34
+numberFour=23.33
 print(int(NumberThree))
 print(int(NumberFour))
 # modify the code below to print an integer number.
 num_one=20
 num_two=23.45
 
-print(num_one + num_two()
+print(int(num_one + num_two))
 
 # modify the following code below to work correctly.
 name_of_pet="Spuny"
-age_of_pet="10"
+age_of_pet=str(10)#'10'
 
-print("this is my dog",+ name_ of_pet +".He is"+ age_of_pet +"years old.")
+print("this is my dog"+ , name_of_pet +".He is"+ age_of_pet +"years old.")
 
