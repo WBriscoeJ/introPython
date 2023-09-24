d5af2f06bd355ce4e810bdee907f12b1231b792a
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-09-24 00:15:39 +0000

    done functionStarterActivity.py

--- introPythonActivies/functionStarterActivity.py
+++ introPythonActivies/functionStarterActivity.py
@@ -7,12 +7,15 @@
 
 # 3. write a function that will print out a welcome message
 # that includes a users name. You will need to use parameters and arguments
-def welcome_Msg(username)
+#def welcome_Msg(username):
+ print('welcome + username')
+welcome_Msg('wadeBriscoe')
 
 # 4. Write a function that will do a calculation that takes 3 parameters.
 # Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
+The operater Im using is Logic operaters my function is Parmeter=Placeholder.
 
 # 5. Write a function that will output a message to a user, telling them
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
-# be real, user data- not hard coded data.  
+# be real, user data- not hard coded data.  I need help with this question
