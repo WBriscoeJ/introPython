a7f7b2d71bc416438e582d009d3fd31398b8ec83
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-09-25 16:13:01 +0000

    DONE functionActivity.py

--- introPythonActivies/functionStarterActivity.py
+++ introPythonActivies/functionStarterActivity.py
@@ -1,5 +1,5 @@
 
-# 1. In your own words, describe what a function is  something that runs when it called 
+# 1. In your own words, describe what a function is . something that runs when it called 
 
 # 2. What  are function parameters and arguments and describe
 # the difference between the 2. Parameter is a placeholder and argument is when you make an argument 
@@ -7,15 +7,21 @@
 
 # 3. write a function that will print out a welcome message
 # that includes a users name. You will need to use parameters and arguments
+'
+
 #def welcome_Msg(username):
- print('welcome + username')
+print('welcome + username')
 welcome_Msg('wadeBriscoe')
 
 # 4. Write a function that will do a calculation that takes 3 parameters.
 # Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
-The operater Im using is Logic operaters my function is Parmeter=Placeholder.
+The operater Im using is arithmetic operations 2+7
 
 # 5. Write a function that will output a message to a user, telling them
 # what class they have next after this one. this code should use a 
 # variable to pass a value into the parameter of a function. The variable should
-# be real, user data- not hard coded data.  I need help with this question
+# be real, user data- not hard coded data.  
+
+def userMsg():
+nextclass= input('what is your next class.')
+print("you have "+ nextClass +"after this")
