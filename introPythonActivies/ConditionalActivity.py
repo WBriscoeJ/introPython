9bca20eda8efb27dc66abdc53ab8b32f15b6bdb1
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-09-28 15:45:15 +0000

    done ConditionalActivity.py

---
+++ introPythonActivies/ConditionalActivity.py
@@ -0,0 +1,39 @@
+# 1. What is the difference between 
+# a parameter and an argument in a python function
+
+"A parameter is the variable listed inside the parentheses in the function"
+"definition. An argument is the value that are sent to the function when it is called"
+
+# 2. In your own words, describe what 
+# a conditional statement (if/else) is 
+
+"executes a block of code if a specified condition is true."
+
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. 
+The operators `>`, `<`, `>=`, `<=` compare the order of values.
+They determine if a value or greater than or less than another value.
+
+# 4. Write a conditional statement for food in the refridgerator.
+# your conditional statement should be wrapped in a function that takes one (1)
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
+# If there is food in the refridgerator, print time to cook. If there is 
+# NOT food in the fridge, print time to shop. 
+# When you call your function there should be an argument that accepts 
+# a boolean. 
+
+# We know that it talks about functions,parameter,data type ,print,and def
+# IF food_in_refridgerator=True
+# Else no_food_in_refridgerator=False
+
+def put_food_in_refridgerator(food_in_refridgerator):
+    food_in_fridg=input('pleas enter type of foods')
+        if food_in_fridg:foods_in_refridgerator=='Ham burger,fruit,and water'   
+ else:noFoodsInRefridgerator==empty refridgerator.
+
+
+# 5. Create a function that checks the inventory of cereal for a store. 
+# your function should have a parameter that accepts an integer. In your function
+# use a conditional statement to determine if you need to order more cereal.
+# If there is more than 10 boxes, print "inventory full", else if there are less than 
+# 10 boxes print "we need to order more cereal".
