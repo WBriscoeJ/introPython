00a25eb23c164e96fbeba48312dbc6677568c160
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-10-18 16:56:49 +0000

    done loop practice

---
+++ introPythonActivies/loopReviewActivity.py
@@ -0,0 +1,21 @@
+# write loop a that will work for these three scenarios
+
+# passcode Loop
+
+# Printer Loop
+
+# Stop Light Loop
+
+
+def continueExample():
+   i=0
+   while i <= 8:
+       i+=1
+       if i==5:
+           print('skip the number 5.')
+           continue
+       print(i)
+
+   # print('our loop is done.)
+
+continueExample()
