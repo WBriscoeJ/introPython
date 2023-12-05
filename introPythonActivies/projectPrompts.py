a76a16ad8022c94c9b5c6fb50e3f2daf7fa04375
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-12-05 16:32:43 +0000

    Done project prompt

--- introPythonActivies/projectPrompts.py
+++ introPythonActivies/projectPrompts.py
@@ -1,11 +1,10 @@
 
 import random
 
-a = 1
+a = "yes"
 
-while a == 1:
-
-    number= random.randint(1,6)
+while a == "yes":
+    number = random.randint(1, 6)
 
     if number == 1:
         print("[-----]") 
@@ -49,7 +48,35 @@ while a == 1:
         print("[0 0 0]")
         print("[-----]")    
 
-    a= input(" press space bar twice to continue")
+    print(number)
+    a = input("Do you want to continue? yes or no")
 
-    print('random number')
 
+#prints a random value from the list
+my_list = [1,2,3,4,5,6]
+print('Random number from list:', random.choice('1,2,3,4,5,6,'))
+print('Random number from list:', random.choice('1,2,3,4,5,6,'))
+
+
+
+
+
+1. #breifly describe what your program is about?It supose to repeat it by useing random numbers.
+
+
+2. #What probblem is your program trying to solve?The problem its trying to solve is do you 
+#want to continue or not.
+
+3. #How will your program solve this problem? It would solve this problem by choosing Yes or No to 
+#continue or not.
+
+4. #Are there any apps that similar to your program?yes there is a game that is called bored king
+
+
+5. #Who is this program designed for? Please describe the user who will be
+   #useing the program by describing three(3) things about them and how it relates to them 
+   #using the program.
+#The program is use for the public/everybody is free to use this.The user who would use this is my
+#teacher.Three thing's about them and how it relates using this program is by Mr.Ian having to come
+#to work every day accept monday,he also comes to work 4 days out of the week,and he acyually does those
+#thing's.
