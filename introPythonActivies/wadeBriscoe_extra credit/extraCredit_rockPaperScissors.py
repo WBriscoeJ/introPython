4bd7ca6f52399732124a29369e4f2a58b407a3d9
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2023-12-12 16:42:09 +0000

    done extra credit work

---
+++ introPythonActivies/wadeBriscoe_extra credit/extraCredit_rockPaperScissors.py
@@ -0,0 +1,22 @@
+import random
+
+def makeYourChoice():
+    print ("press r for Rock")
+    print ("press p for Paper")
+    print ("press s for Scissors")
+    print("press q to Quit!")
+    userChoice = raw_input("what do you want to choose?")
+
+    if userChoice == "r":
+        return "Rock"
+    if userChocie == "p":
+        return "Paper"
+    if userChoice == "s":
+        return "Scissors"
+    if userChoice == "q":
+        sys.exit (0)
+    else:
+        makeYourChoice()
+
+
+    
