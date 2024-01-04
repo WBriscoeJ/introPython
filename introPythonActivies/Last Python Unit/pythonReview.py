a4a49202a50af2e1aa476a2906e10d2c745e1d04
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2024-01-04 17:54:52 +0000

    done python review

---
+++ introPythonActivies/Last Python Unit/pythonReview.py
@@ -0,0 +1,10 @@
+1)"A set of instruction that tells a computer specifc order."
+
+"Floating Point (float)	Numeric data type for numbers with fractions,
+string (str or text) a 	Sequence of characters, digits, or symbols—always treated as text"
+Boolean (bool)	True or false values
+
+
+I would use string if I'm typing  a function.I would use boolean for a true or false statement.I would
+float for numeric data.
+
---
+++ introPythonActivies/Last Python Unit/pythonReviewActivity.py
@@ -0,0 +1,8 @@
+1)The operator I would use is Arithemetic opraters.I would use Arithemetic because it uses subtraction,
+addition,multiplication,and division.
+
+2)The operater I would use is assignment operater.I would assignment operater because it
+assigns the value of its right-hand operand to a variable.
+
+3)The operater I would use is logical operater.I would use logical operater to connect 
+two or more expressions.
