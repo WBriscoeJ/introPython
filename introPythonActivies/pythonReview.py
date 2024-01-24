782b27162cc03106094b5c3f665ec69cda260fab
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2024-01-24 18:00:22 +0000

    done pythonReview

---
+++ introPythonActivies/pythonReview.py
@@ -0,0 +1,44 @@
+# Sve this as pythonReview.py
+
+# In your own words describe, what a computer program is?
+A computer program is a sequence or a set of instructions in a programming language  for a computer to 
+execute.
+
+
+
+
+# In your own words, describe what it mean to call a function? 
+In my own word calling a function gives commands so the that  computer  will run  the code for that 
+
+function.
+
+
+# Create a elevator function that will run specific lines of code 
+# based on the conditions provided by a user. If the user types in 101,
+# the function should print out they are going to the boys latin office, 
+# if they type in 203, they are going to the gym, 
+# and if they type in the letter g, the lobby. else, 
+# if the input doesnt match any of the values provided, 
+# tell the user that floor doesn't exist and to please
+# enter a valid floor number.
+
+#def      
+
+
+
+
+# Create a function that will do the following calculation.
+# Your function should take in three argument. it should add the first
+# two arguments and then the sum of the first two (2) should be divided 
+# by the third argument. You function should then print theresult.
+    
+num = int(input("Enter a number:"))
+if range_test(num):
+    print("{:?} is in range.".format(num))
+else:
+    print(The number you entered is outside the range) 
+num(7)
+
+
+
+
