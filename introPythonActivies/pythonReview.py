4459edc566d63254f30ae2687f004a2a0eba6664
WBriscoeJ <144711122+WBriscoeJ@users.noreply.github.com>
2024-01-25 16:47:40 +0000

    done

--- introPythonActivies/pythonReview.py
+++ introPythonActivies/pythonReview.py
@@ -1,16 +1,16 @@
 # Sve this as pythonReview.py
 
 # In your own words describe, what a computer program is?
-A computer program is a sequence or a set of instructions in a programming language  for a computer to 
-execute.
+ #A computer program is a sequence or a set of instructions in a programming language  for a computer to 
+ #execute.
 
 
 
 
 # In your own words, describe what it mean to call a function? 
-In my own word calling a function gives commands so the that  computer  will run  the code for that 
+ #In my own word calling a function gives commands so the that  computer  will run  the code for that 
 
-function.
+ #function.
 
 
 # Create a elevator function that will run specific lines of code 
@@ -32,12 +32,11 @@ function.
 # two arguments and then the sum of the first two (2) should be divided 
 # by the third argument. You function should then print theresult.
     
-num = int(input("Enter a number:"))
-if range_test(num):
-    print("{:?} is in range.".format(num))
-else:
-    print(The number you entered is outside the range) 
-num(7)
+def customCalulation(arg1, arg2, arg3):
+    result = (arg1+arg2)/arg3
+    print(result)
+
+customCalulation(1,2,3##)
 
 
 
